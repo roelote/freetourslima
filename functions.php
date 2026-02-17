2225c5c72f84275a97a12fc281ae98852efc27bc
Roel Jhonatan <jroelx@gmail.com>
2026-02-16 19:14:44 -0500

    cambios web edicion

--- functions.php
+++ functions.php
@@ -542,3 +542,26 @@ function cf7_set_hidden_fields() {
     <?php
 }
 add_action('wp_footer', 'cf7_set_hidden_fields');
+
+if (function_exists('acf_add_options_page')) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+}
