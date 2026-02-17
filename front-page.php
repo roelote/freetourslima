a18d799c0c7fff985df74d78e57888320e678db1
Roel Jhonatan <jroelx@gmail.com>
2026-02-16 20:10:23 -0500

    c

--- front-page.php
+++ front-page.php
@@ -35,7 +35,7 @@ if (ICL_LANGUAGE_CODE == 'en') {
                             </form>
                         </div>
                     </div>
-                    <div>
+                    <div >
                         <div class="w-full flex flex-col gap-[8px] md:gap-[30px] mt-0 md:mt-[105px]">
                             <!-- Details Card -->
                             <div class="bg-[#efede7] border border-[#dad9d6] rounded-[8px] p-[20px]">
