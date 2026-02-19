ba377c68c61f5957df6d9b02313589d16ce28991
Roel Jhonatan <jroelx@gmail.com>
2026-02-18 21:57:01 -0500

    correccion 15

--- front-page.php
+++ front-page.php
@@ -36,8 +36,8 @@ if (ICL_LANGUAGE_CODE == 'en') {
                                 <input type="hidden" name="urlfoto" value="<?= urlencode(the_post_thumbnail_url()) ?>">
                                 <input type="hidden" name="nametour" value="<?= the_title(); ?>">
                                 <div id="calendar-inline" class="w-full"></div>
-                                <input type="text" id="date-selected" name="date" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Selected Date' : 'Fecha seleccionada'; ?>" class="w-full p-3 rounded-[8px]" readonly>
-                                <input type="number" name="personas" class="w-full p-3 rounded-[8px]" min="0" max="30" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'People' : 'Personas'; ?>">
+                                <input type="text" id="date-selected" name="date" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Selected Date' : 'Fecha seleccionada'; ?>" class="w-full px-3 py-[9px] rounded-[8px]" readonly>
+                                <input type="number" name="personas" class="w-full px-3 py-[9px] rounded-[8px]" min="0" max="30" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'People' : 'Personas'; ?>">
                                 <button type="submit" class="w-[113px] rounded-[8px]  py-[18px] text-[16px] font-bold leading-[20px] text-white"><?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Book Now' : 'Reservar'; ?></button>
                             </form>
                         </div>
