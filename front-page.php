9e689beb9a6933379e70da4a9377ae63b9d4eb50
Roel Jhonatan <jroelx@gmail.com>
2026-02-19 21:21:16 -0500

    texto input

--- front-page.php
+++ front-page.php
@@ -37,7 +37,7 @@ if (ICL_LANGUAGE_CODE == 'en') {
                                 <input type="hidden" name="nametour" value="<?= the_title(); ?>">
                                 <div id="calendar-inline" class="w-full"></div>
                                 <input type="text" id="date-selected" name="date" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Selected Date' : 'Fecha seleccionada'; ?>" class="w-full px-3 py-[9px] rounded-[8px]" readonly>
-                                <input type="number" name="personas" class="w-full px-3 py-[9px] rounded-[8px]" min="0" max="30" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'People' : 'Personas'; ?>">
+                                <input type="number" name="personas" class="w-full px-3 py-[9px] rounded-[8px]" min="0" max="30" placeholder="<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Nuumber of attendees' : 'Número de asistentes'; ?>">
                                 <button type="submit" class="w-[113px] rounded-[8px]  py-[18px] text-[16px] font-bold leading-[20px] text-white"><?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Book Now' : 'Reservar'; ?></button>
                             </form>
                         </div>
