d0afdedc567e435b4b6299e4f3c761e2a6ccfb71
Roel Jhonatan <jroelx@gmail.com>
2026-02-20 19:30:47 -0500

    mt

--- front-page.php
+++ front-page.php
@@ -37,7 +37,7 @@ if (ICL_LANGUAGE_CODE == 'en') {
                         </div>
                     </div>
                     <div class="">
-                        <div class="w-full flex flex-col gap-[8px] md:gap-[30px] mt-0 md:mt-[13px] sticky top-[20px]">
+                        <div class="w-full flex flex-col gap-[8px] md:gap-[30px] mt-0 md:mt-[76px] sticky top-[20px]">
                             <!-- Details Card -->
                             <div class="bg-[#efede7] border border-[#dad9d6] rounded-[8px] p-[20px]">
                                 <div class="flex flex-col gap-[12px]">
