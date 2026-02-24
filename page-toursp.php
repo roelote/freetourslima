013456d4020de94706c07c7b27c7f5fd3407e86e
Roel Jhonatan <jroelx@gmail.com>
2026-02-23 20:08:06 -0500

    mejora h2

--- page-toursp.php
+++ page-toursp.php
@@ -49,7 +49,7 @@ if (ICL_LANGUAGE_CODE == 'en') {
                 </div>
             </div>
             <div class="w-full md:w-[374px]">
-                 <div class="w-full mt-0 md:mt-[8.5rem] flex flex-col-reverse md:flex-col gap-[32px] md:gap-[100px]">
+                 <div class="w-full mt-0 md:mt-[8.2rem] flex flex-col-reverse md:flex-col gap-[32px] md:gap-[100px]">
                     <div class="w-full">
                         <div class="calendar-wrapper">
                             <form class="flex flex-col gap-[16px]" action="<?= $ruta ?>" method="get" id="bookingForm">
