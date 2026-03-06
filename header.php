9d7d6ce58f1c29c167ef5a83e7fc8f9f6a3034ec
Roel Jhonatan <jroelx@gmail.com>
2026-03-05 21:00:54 -0500

    menus14

--- header.php
+++ header.php
@@ -308,13 +308,13 @@
             <?php else : ?>
                 <!-- Usuario no logueado -->
                 <div class="flex flex-col gap-5 mt-4">
-                    <button id="mobile-open-login-modal" class="flex items-center justify-center gap-2 w-full px-4 py-[1.1rem] rounded-lg  bg-[#FEB370]  text-white font-medium text-sm hover:bg-[#1ab6b6] transition-colors">
+                    <button id="mobile-open-login-modal" class="flex items-center justify-center gap-2 w-full px-4 py-[1.1rem] rounded-lg  bg-[#FEB370]  text-white font-medium text-base hover:bg-[#1ab6b6] transition-colors">
                         <svg class="w-5 h-5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
                         </svg>
                         <?php echo fw_trans('login_title'); ?>
                     </button>
-                    <button id="mobile-open-register-modal" class="flex items-center justify-center gap-2 w-full px-4 py-[1rem] rounded-lg border-2 border-[#FEB370] text-[#FF8110] font-medium text-sm hover:bg-[#1ab6b6] hover:border-[#1ab6b6] hover:text-white transition-colors">
+                    <button id="mobile-open-register-modal" class="flex items-center justify-center gap-2 w-full px-4 py-[1rem] rounded-lg border-2 border-[#FEB370] text-[#FF8110] font-medium text-base hover:bg-[#1ab6b6] hover:border-[#1ab6b6] hover:text-white transition-colors">
                         <svg class="w-5 h-5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
                         </svg>
